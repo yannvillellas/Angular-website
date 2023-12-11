082b518158e4145c1a726ce0ac1b12306ec2abe1
Yann <yvillellas@gmail.com>
2023-12-11 15:26:35 +0100

    eagle logo pages navbar

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LessonsComponent} from "./lessons/lessons.component";
+import {AboutComponent} from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"lessons", component: LessonsComponent},
+  {path:"about", component: AboutComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
