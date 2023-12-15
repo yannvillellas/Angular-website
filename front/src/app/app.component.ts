2d0a4c9b18f68496704c3e6fcfed8b2ca8513ba3
yannvillellas <yvillellas@gmail.com>
2023-12-15 23:04:09 +0100

    moved lessons into correct page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { LearningPackageService } from './learning-package.service';
 
 @Component({
@@ -6,30 +6,6 @@ import { LearningPackageService } from './learning-package.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'angular-website';
-  learningPackages: any=[];
-  packageTitle: any;
+export class AppComponent{
 
-  constructor(private learningPackageService: LearningPackageService) { }
-  addNewLearningPackage(packageTitle: string): void {
-    const newPackage = { title: packageTitle }; // Créez l'objet package avec le titre
-    this.learningPackageService.addLearningPackage(newPackage).subscribe(
-      data => {
-        console.log('Package added:', data);
-        this.learningPackages.push(data); // Ajoutez le nouveau package à la liste
-      },
-      error => console.error('Error adding package:', error)
-    );
-  }
-  ngOnInit(): void {
-    this.learningPackageService.getLearningPackages().subscribe(data => {
-      console.log(data);
-      this.learningPackages = data;
-    });
-
-
-
-
-  }
 }
--- front/src/app/lessons/lessons.component.ts
+++ front/src/app/lessons/lessons.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LearningPackageService} from "../learning-package.service";
 
 @Component({
   selector: 'app-lessons',
   templateUrl: './lessons.component.html',
   styleUrls: ['./lessons.component.css']
 })
-export class LessonsComponent {
+export class LessonsComponent implements OnInit{
+  title = 'angular-website';
+  learningPackages: any=[];
+  packageTitle: any;
 
+  constructor(private learningPackageService: LearningPackageService) { }
+  addNewLearningPackage(packageTitle: string): void {
+    const newPackage = { title: packageTitle }; // Créez l'objet package avec le titre
+    this.learningPackageService.addLearningPackage(newPackage).subscribe(
+        data => {
+          console.log('Package added:', data);
+          this.learningPackages.push(data); // Ajoutez le nouveau package à la liste
+        },
+        error => console.error('Error adding package:', error)
+    );
+  }
+  ngOnInit(): void {
+    this.learningPackageService.getLearningPackages().subscribe(data => {
+      console.log(data);
+      this.learningPackages = data;
+    });
+  }
 }
