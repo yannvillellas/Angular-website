e4e5fcda81b0844c6716307de98cd9fec253f5af
Yann <yvillellas@gmail.com>
2023-12-18 17:42:30 +0100

    add homepage

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LessonsComponent} from "./lessons/lessons.component";
 import {AboutComponent} from "./about/about.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
+  {path:"", component: HomeComponent},
   {path:"lessons", component: LessonsComponent},
   {path:"about", component: AboutComponent}
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LessonsComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
