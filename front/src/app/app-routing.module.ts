52b45fe8036eb9e7806d0025897cd79d3ad61999
Yann <yvillellas@gmail.com>
2023-12-26 18:28:55 +0100

    created button to go

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LessonsComponent} from "./lessons/lessons.component";
-import {AboutComponent} from "./about/about.component";
 import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {LessonsComponent} from "./lessons/lessons.component";
+import {QuestionsComponent} from "./questions/questions.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "lessons", component: LessonsComponent },
-  { path: "lessons/:id", component: LessonsComponent},
+  { path: "lessons/:id", component: QuestionsComponent},
   { path: "not-found", component: NotFoundComponent },
   { path: "**", redirectTo: "not-found" } // Redirect all other routes to the 404 page
 ];
