1d1431f1b7a70ec8e14194afb1f4dd8a03f47fd6
Guillaumeyss <114230433+Guillaumeyss@users.noreply.github.com>
2023-12-28 16:59:15 +0100

    Grid implemented

--- front/src/app/about/about.component.ts
+++ front/src/app/about/about.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ColDef} from "ag-grid-community";
+import {HttpClient} from "@angular/common/http";
+import {LearningPackage} from "../learning_package.model";
+
+import {Observable, take} from "rxjs";
+import {LearningPackageService} from "../learning-package.service";
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
-rowData:any[]=[
-  {price:10},
-  {price:20}
-];
-colDefs:ColDef[]=[
-  {field:'price'}
-];
+export class AboutComponent implements OnInit {
+    rowData$: Observable<LearningPackage[]>;
+    colDefs: ColDef[] = [{field: 'title', headerName:'Lesson',width: 250 },
+        {field: 'description', headerName:'Description',width: 400 },
+        {field: 'category', headerName:'Category'},
+        {field: 'targetAudience', headerName:'Audience'},
+        {field: 'id', headerName:'ID',width: 250}];
+    learningPackages: LearningPackage[] = [];
+    private gridOptions: any = {
+        domLayout: 'autoHeight',
+        autoWidth: true,
+        suppressHorizontalScroll: false
+    };
+    onGridReady(params: any) {
+        this.gridOptions = params.api;
+        this.gridOptions.api.sizeColumnsToFit();
+    }
+
+    constructor(private learningPackageService: LearningPackageService) { }
+
+
+    ngOnInit(): void {
+
+        this.rowData$ = this.learningPackageService.getLearningPackages();
+        this.rowData$.subscribe(data => {
+            this.gridOptions.api.sizeColumnsToFit();
+        });
+    }
+
+   
 }
+
